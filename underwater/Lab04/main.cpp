61ac07face396865a41fd099beb2bd9343988830
ghostyusheng <525741775@qq.com>
2024-11-06 20:40:23 +0100

    dae模型纹理载入调试

--- underwater/Lab04/main.cpp
+++ underwater/Lab04/main.cpp
@@ -550,11 +550,11 @@ void init() {
     // Load and bind the texture for particles
     particleTextureID = loadTexture("test.jpg");
 
-    models.push_back(load_model("monkey.dae", vec3(0.0f, 0.0f, -10.0f), -45.0f));
+    models.push_back(load_model("pink_cube.dae", vec3(0.0f, 0.0f, -10.0f), 30.0f));
     models.push_back(load_model("monkey.dae", vec3(0.0f, 5.0f, -10.0f), -45.0f));
 
     // Initialize multiple fish models
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 1; ++i) {
         FishModel fish;
         fish = load_fish_model("simple_fish6.dae", vec3(0.0f, i * -2.0f, -10.0f), 45.0f);
         fish.direction = vec3(get_rand(1,10), i, 0.0f); // Set initial swimming direction
